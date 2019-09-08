b8e8b2dde99074368e5d983cc44709f82ee7686b
Jefferson costa Oliveira <45311907+JeffersonCO@users.noreply.github.com>
2019-09-08 19:52:50 -0300

    Questoes do teste

---
+++ Questao 1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao_1
+{
+    class Program
+    {
+      //Função recursiva para calcular um numero fatorial.
+        public int Calcular(int numero)
+        {
+            int resultado;
+            //Como parametro de parada da função foi colocado que o numero seja zero
+            //porem se for esse o caso a função vai sempre retornar 0 pois segundo as leis matematicas 
+            //todo numero multiplicado por zero e zero, sendo assim o menor numero possivel para retorno de uma 
+            //função fatorial seria o numero 1.
+            if (numero <= 1)
+            {
+                return (1);
+            }
+            //Cjamada da função formando uma pilha de operações a serem executadas apos a parada da função.
+            else
+            {
+                resultado = numero * (Calcular(numero - 1));
+                return resultado;
+            }
+        }
+        public void Main()
+        {//Metodo  para chamar a função e apresentar o resultado a partir de um numero pré estabelecido .
+            int numero = 5;
+            int resultado = Calcular(numero);
+
+            Console.WriteLine("Resultado :{0}", resultado);
+            Console.ReadKey();
+        }
+
+    }
+}
+ 
+        
+    
+    
+
+
---
+++ Questao 2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            //Declarando as variaveis .
+            string name;
+            string[,] matriz;
+            char[] splitName = new char[1];
+            Console.BackgroundColor = ConsoleColor.Red;
+            //Entrada do programa, Digitar o nome a ser inserido na Matriz.
+            Console.WriteLine("Escreva o nome a ser inserido na matriz: ");
+            Console.WriteLine();
+          
+            //Convertendo a string em um array de caracter.
+            Console.ForegroundColor = ConsoleColor.White;
+            name = Console.ReadLine();
+            Console.WriteLine();
+            splitName = name.ToCharArray();
+            matriz = new string[3, name.Length + 3];
+            int n1 = 0;
+
+            //Criação da implementação na Matriz
+            //È necessario criar linha por linha da matriz
+            //Eixo horizontal ou linha  da Matriz .
+            for (int i = 0; i < 3; i++)
+            {
+                //Eixo vertical ou coluna da Matriz .
+                for (int k = 0; k < name.Length * 3; k++)
+                {
+                    //Atribuindo Valores dentro dos campos da Matriz na primeira Linha.
+                    if (i < 1 && k < name.Length + 2)
+                    {
+                        n1++;
+                        string n2 = Convert.ToString(n1);
+                        Console.Write(matriz[i, k] = n2 + "\t");
+                    }
+                    //Passando para proxima linha.
+                    else if (i < 1 && k == name.Length + 2)
+                    {
+                        Console.WriteLine();
+                    }
+
+                    //Colocando valor na primeira posição e segunda linha da matriz.
+                    if (i == 1 && k < 2)
+                    {
+                        int n3 = n1;
+                        n3 = (name.Length + 2) * 2 + 2;
+                        string n2 = Convert.ToString(n3);
+                        Console.Write(matriz[i, k] = n2 + "\t");
+                    }
+
+                    //Atribuindo Valores na Segunda linha da Matriz.
+                    if (i == 1 && k < name.Length - 1)
+                    {
+                        n1++;
+                        string n2 = Convert.ToString(n1);
+                        int a = -1;
+
+                        //Atribuindo o Nome escolhido na Matriz.
+                        while (a < name.Length - 1)
+                        {
+                            a++;
+                            Console.Write(splitName[a].ToString() + "\t");
+                        }
+
+                        //Atribuindo o Valor na Ultima coluna da Segunda linha.
+                        if (i == 1)
+                        {
+                            n2 = Convert.ToString(n1);
+                            Console.Write(matriz[i, k] = n2);
+                        }
+
+                        //Atribundo Valores da Ultima linha da Matriz.
+                        Console.WriteLine();
+                        int n4 = (name.Length + 2) * 2 + 2;
+
+                        for (int y = 0; y < name.Length + 2; y++)
+                        {
+                            n4--;
+                            n2 = Convert.ToString(n4);
+                            Console.Write(matriz[i, k] = n2 + "\t");
+                        }
+                        break;
+                    }
+                }
+            }
+            
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
